9d0e69c3b19b652d75b1e9b1d19e04bb652617f2
Marius Smytzek <marius.smytzek@cispa.de>
2026-02-04 09:35:38 +0100

    added sanic fix

--- pwfl/utils.py
+++ pwfl/utils.py
@@ -11,9 +11,9 @@ def fix_sanic(project, original_checkout):
             os.path.join(original_checkout, "tests4py_requirements.txt"), "w"
         ) as f:
             f.write(
-                content.replace(
-                    "requests-async==0.4.1", "http3==0.6.*\nrequests==2.*"
-                ).replace("requests-async==0.5.0", "http3==0.6.*\nrequests==2.*")
+                content.replace("requests-async==0.4.1", "http3==0.6.*\nrequests==2.*")
+                .replace("requests-async==0.5.0", "http3==0.6.*\nrequests==2.*")
+                .replace("chardet==2.3.0", "chardet==3.0.4")
             )
         if project.bug_id == 4:
             with open(os.path.join(original_checkout, "setup.py"), "r") as f:
